d5da004a2f3d6246b4db2c265bbe186751689b9a
Jack Taylor <jackmac@Jacks-MacBook-Pro.local>
2020-02-21 11:50:40 -0500

    fixed all the errors and changed vectors from chars to strings :)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
+
 
 // example input: TAATGACCCACGAATAAGCTTTTAAGTACTAGCGAACATTCTATGTTCTTGGCTAGTCACTAATTAATTAACTAATCAT
 
@@ -13,9 +16,34 @@ using namespace std;
 // RNA sequence
 // amino acid sequence
 // tRNA sequence
-int main() 
+
+vector<string> readFile(){
+    vector<string> input; //get rid of this eventually
+    input.push_back("A");
+    input.push_back("T");
+	input.push_back("G");
+    input.push_back("C");
+    //here pull in a text file, store it in input
+
+    return input;
+}
+
+void outputRNASequence(vector<string> input){
+    cout << "output\n";
+
+}
+void outputAASequence(vector<string> input){
+    cout << "output\n";
+
+}
+void outputTRNASequence(vector<string> input){
+    cout << "output\n";
+
+}
+
+int main()
 {
-    vector<char> input = readFile();
+    vector<string> input = readFile();
 
     // ask if it's from 3' to 5' or 5' to 3'
     // if it's 3' to 5', reverse input
@@ -23,27 +51,8 @@ int main()
 
     outputRNASequence(input);
     outputAASequence(input);
-    outputTRNASequence(input); 
+    outputTRNASequence(input);
     return 0;
 }
 
-vector<char> readFile(){
-	char input[] = { 'A', 'G', 'T', 'C', 'A' }; //get rid of this eventually
 
-	//here pull in a text file, store it in input
-
-	return input
-}
-
-void outputRNASequence(vector<char> input){ 
-	cout << "output"
-
-}
-void outputAASequence(vector<char> input){ 
-	cout << "output"
-
-}
-void outputTRNASequence(vector<char> input){ 
-	cout << "output"
-
-}
