33f5a04cc4145d8cf03f6508f0acd5b97eaf18fd
s-almeda <almedas1@tcnj.edu>
2020-02-21 11:37:15 -0500

    skeleton code

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // example input: TAATGACCCACGAATAAGCTTTTAAGTACTAGCGAACATTCTATGTTCTTGGCTAGTCACTAATTAATTAACTAATCAT
 
 //TASKS:
-// read input from a text file into a string
+// read input from a text file into a character array
 // find ORF
 // scan through codon by codon
 // stop when stop codon is found
@@ -15,6 +15,35 @@ using namespace std;
 // tRNA sequence
 int main() 
 {
-    cout << "Hello, World!";
+    vector<char> input = readFile();
+
+    // ask if it's from 3' to 5' or 5' to 3'
+    // if it's 3' to 5', reverse input
+
+
+    outputRNASequence(input);
+    outputAASequence(input);
+    outputTRNASequence(input); 
     return 0;
+}
+
+vector<char> readFile(){
+	char input[] = { 'A', 'G', 'T', 'C', 'A' }; //get rid of this eventually
+
+	//here pull in a text file, store it in input
+
+	return input
+}
+
+void outputRNASequence(vector<char> input){ 
+	cout << "output"
+
+}
+void outputAASequence(vector<char> input){ 
+	cout << "output"
+
+}
+void outputTRNASequence(vector<char> input){ 
+	cout << "output"
+
 }
