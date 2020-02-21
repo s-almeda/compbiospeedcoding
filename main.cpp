115797973528baac081f7c2d84936c578e5df423
s-almeda <almedas1@tcnj.edu>
2020-02-21 11:26:50 -0500

    I added comments on tasks

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
+// example input: TAATGACCCACGAATAAGCTTTTAAGTACTAGCGAACATTCTATGTTCTTGGCTAGTCACTAATTAATTAACTAATCAT
+
+//TASKS:
+// read input from a text file into a string
+// find ORF
+// scan through codon by codon
+// stop when stop codon is found
+// 
+// OUTPUT:
+// RNA sequence
+// amino acid sequence
+// tRNA sequence
 int main() 
-{ //change
+{
     cout << "Hello, World!";
     return 0;
 }
